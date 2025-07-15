61cdacce9ea8792f3ba41775df3456828d4b6469
nadirboudia <n_boudia@estin.dz>
2025-07-15 15:29:00 +0100

    services

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Slider from './components/slider/Slider'
+import Services from "./components/Services/Services"
 
 function App() {
   return (
     <div>
       <Header />
       <Slider/>
+      <Services/>
     </div>
   );
 }
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,27 @@
+import './Services.css'
+
+function Services() {
+  return (
+    <div className='services'>
+      <div className="service-item">
+        <i className="bi bi-truck"></i>
+        <b>Free Shipping</b>
+      </div>
+      <div className="service-item">
+        <i className="bi bi-gift"></i>
+        <b>Gift Card</b>
+      </div>
+      <div className="service-item">
+        <i className="bi bi-arrow-clockwise"></i>
+        <b> 7 Days return</b>
+      </div>
+      <div className="service-item">
+        <i className="bi bi-truck"></i>
+        <b>Contact Us </b>
+      </div>
+
+    </div>
+  )
+}
+
+export default Services
